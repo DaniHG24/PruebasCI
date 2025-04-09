98892b3f74cf13769ff1f33411059126503efed1
DaniH <147464176+DaniH2003@users.noreply.github.com>
2025-04-08 18:45:02 -0700

    Add files via upload

---
+++ ExamenU2/PagoTransferencia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenU2
+{
+    public class PagoTransferencia : IPago
+    {
+        public double CalcularPago(double total)
+        {
+            return total * 1.05;
+        }
+    }
+}
